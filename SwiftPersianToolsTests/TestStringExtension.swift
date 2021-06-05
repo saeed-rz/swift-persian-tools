7758ba3df2f531c32919e6b659df9ad0215fb522
Saeedrz <saeed.rezaei.sa@gmail.com>
2021-06-05 13:35:04 -0700

    write unit test for validate national iranian code

--- SwiftPersianToolsTests/TestStringExtension.swift
+++ SwiftPersianToolsTests/TestStringExtension.swift
@@ -29,5 +29,18 @@ class TestStringExtension: XCTestCase {
         XCTAssertEqual(stringNumber.addComma(), "۱۲۳٬۷۸۹")
     }
 
+    func testValidateNationalCode() throws {
+        var codeMelli = "0499370899"
 
+        XCTAssertTrue(codeMelli.isValidIranianNationallCode())
+
+        codeMelli = "0963695398"
+        XCTAssertTrue(codeMelli.isValidIranianNationallCode())
+
+        codeMelli = "0067749828"
+        XCTAssertTrue(codeMelli.isValidIranianNationallCode())
+
+        codeMelli = "0684159415"
+        XCTAssertFalse(codeMelli.isValidIranianNationallCode())
+    }
 }

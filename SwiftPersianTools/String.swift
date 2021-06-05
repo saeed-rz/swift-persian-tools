a2cfc18d04e5d4dae487d63efe5ac7bc205cdb98
Saeed Rezaei <saeed.rezaei.sa@gmail.com>
2021-06-06 01:10:18 +0430

    Feature validate iran national number (#6)
    * implement check  iranian national  code
    
    * write unit test for validate national iranian code

--- SwiftPersianTools/String.swift
+++ SwiftPersianTools/String.swift
@@ -56,4 +56,16 @@ public extension String {
 
         return returnString
     }
+
+    func isValidIranianNationallCode() -> Bool {
+        let digits = self.compactMap { Int(String($0)) }
+        guard digits.count == 10 && digits.count == self.count else {
+            return false
+        }
+
+        let check = digits[9]
+        let sum = digits[0 ..< 9].enumerated().reduce(0) { $0 + $1.element * (10 - $1.offset) } % 11
+
+        return sum < 2 ? check == sum : check + sum == 11
+    }
 }
